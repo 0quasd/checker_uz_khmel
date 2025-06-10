02affd15375b2aed69935777a918342ee0ce70c2
0quasd <157098698+0quasd@users.noreply.github.com>
2025-06-10 18:38:05 +0300

    Update final_bot_alarm_presence_only.py

--- final_bot_alarm_presence_only.py
+++ final_bot_alarm_presence_only.py
@@ -2,12 +2,15 @@ import logging
 import time
 import os
 
-chrome_options = Options()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--window-size=1920,1080")
-chrome_options.add_argument('user-agent=Mozilla/5.0 ...')
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+
 
 driver = webdriver.Chrome(options=chrome_options)
 
